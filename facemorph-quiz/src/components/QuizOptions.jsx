933a7aceb16cbca42b3ec446e7985775e6d0c03e
Zuby Javed <zubyjaved@gmail.com>
2023-05-07 14:22:49 -0700

    set clicked button to red or green based on if user got the question right

--- facemorph-quiz/src/components/QuizOptions.jsx
+++ facemorph-quiz/src/components/QuizOptions.jsx
@@ -1,20 +1,38 @@
-// QuizOptions.js
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-paper';
 
 const QuizOptions = ({ options, handleButtonClick, isCorrect, randomImageValue }) => {
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleButtonPress = (option) => {
+        setSelectedOption(option);
+        handleButtonClick(option);
+        setIsClicked(true);
+    };
+
+    const getButtonStyle = (option) => {
+        if (selectedOption === option) {
+            return isCorrect && option === randomImageValue
+                ? styles.correctButton
+                : styles.incorrectButton;
+        } else if (isClicked && option === randomImageValue) {
+            return styles.correctButton;
+        }
+        return null;
+    };
+
     return (
         <View style={styles.buttonsContainer}>
             {options.map((option, index) => (
                 <Button
                     key={index}
                     mode="outlined"
-                    style={[styles.quizButton, isCorrect && option === randomImageValue ? styles.selectedButton : null]}
-                    onPress={() => handleButtonClick(option)}
-                    disabled={isCorrect}
-                    label={option}
                     textColor='#fff'
+                    style={[styles.quizButton, getButtonStyle(option)]}
+                    onPress={() => handleButtonPress(option)}
+                    disabled={isCorrect || isClicked}
                 >
                     <Text style={styles.text}>
                         {option}
@@ -36,13 +54,19 @@ const styles = StyleSheet.create({
         margin: 5,
         borderRadius: 5,
     },
-
     selectedButton: {
         backgroundColor: '#999',
     },
     text: {
         fontSize: 15,
+        color: '#fff',
     },
+    correctButton: {
+        backgroundColor: 'green',
+    },
+    incorrectButton: {
+        backgroundColor: 'red',
+    }
 });
 
 export default QuizOptions;
