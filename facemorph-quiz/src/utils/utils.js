a76b7bd8159b50f10ea6d8f43ba74a64bc1ea87c
Zuby Javed <zubyjaved@gmail.com>
2023-05-08 16:36:52 -0700

    import all images from the people images folder

--- facemorph-quiz/src/utils/utils.js
+++ facemorph-quiz/src/utils/utils.js
@@ -5,18 +5,25 @@ import { UPLOAD_ENDPOINT, MORPH_ENDPOINT, AUTH_HEADER } from '@env';
 
 import people from '../../assets/people.json';
 
-const peopleImages = {
-    tomCruise: require('../../assets/images/people/tom-cruise.jpeg'),
-    nikolaTesla: require('../../assets/images/people/nikola-tesla.webp'),
-    abrahamLincoln: require('../../assets/images/people/abraham-lincoln.jpeg'),
-    tobeyMaguire: require('../../assets/images/people/tobey-maguire.jpeg'),
+const importAll = (r) => {
+    return r.keys().map((fileName) => ({
+        slug: fileName.replace(/(.*\/)|(\.jpeg$)|(\.webp$)|(\.png$)/g, '').replace(/-/g, ''),
+        module: r(fileName),
+    }));
 };
+
+const images = importAll(require.context('../../assets/images/people', false, /\.(jpeg|webp|png)$/));
+
+const peopleImages = images.reduce((acc, curr) => {
+    acc[curr.slug] = curr.module;
+    return acc;
+}, {});
 
 // Gets a random image, uploads to server, and returns the image url
 const getRandomImage = async () => {
     const randomIndex = Math.floor(Math.random() * people.length);
     const randomFace = people[randomIndex]; // returns random face object from faces.json
-    const randomFaceKey = randomFace.img_require;
+    const randomFaceKey = randomFace.img_require.toLowerCase(); // Convert to lowercase
 
     const randomFaceAsset = Asset.fromModule(peopleImages[randomFaceKey]);
     await randomFaceAsset.downloadAsync();
@@ -29,7 +36,7 @@ const getRandomImage = async () => {
     return {
         randomImageUrl,
         randomImageData,
-    }
+    };
 };
 
 const uploadImage = async (img) => {
