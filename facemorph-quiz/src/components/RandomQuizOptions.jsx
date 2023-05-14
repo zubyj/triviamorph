9731d1d0e2eb8e304c789160168ecb14c580e6a6
Zuby Javed <zubyjaved@gmail.com>
2023-05-13 23:45:32 -0700

    set background color to red on incorrect answer submit

--- facemorph-quiz/src/components/RandomQuizOptions.jsx
+++ facemorph-quiz/src/components/RandomQuizOptions.jsx
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'green',
     },
     incorrectButton: {
-        borderColor: 'red',
+        backgroundColor: 'red',
     },
     unselectedCorrectButton: {
         backgroundColor: 'green',
